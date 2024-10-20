125e3ad52405a09bed81ff6526b8ea8899a57e9c
asanjinez <alejandro.sanjinez1@gmail.com>
2024-10-20 11:33:55 -0300

    commit inicial - mvn + mysql db

---
+++ src/main/java/com/techforb/challenge/TechforbChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.techforb.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechforbChallengeApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TechforbChallengeApplication.class, args);
+		System.out.println("Hola mundo");
+	}
+
+}
---
+++ src/test/java/com/techforb/challenge/TechforbChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techforb.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechforbChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
