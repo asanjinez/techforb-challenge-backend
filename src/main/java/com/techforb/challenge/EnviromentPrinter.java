0db1bb83236a52af0e7009463e35990c26ce1ad9
asanjinez <alejandro.sanjinez1@gmail.com>
2024-10-28 17:17:44 -0300

    docker changes

--- src/main/java/com/techforb/challenge/EnviromentPrinter.java
+++ src/main/java/com/techforb/challenge/EnviromentPrinter.java
@@ -8,14 +8,11 @@ public class EnviromentPrinter implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        String dbUrl = System.getenv("DB_URL");
-        String dbUsername = System.getenv("DB_USERNAME");
-        String dbPassword = System.getenv("DB_PASSWORD");
-
-        // Registrando las variables de entorno
-        System.out.println("DB_URL: " + dbUrl);
-        System.out.println("DB_USERNAME: " + dbUsername);
-        System.out.println("DB_PASSWORD: " + dbPassword);
+        System.out.println("Imprimiendo variables de entorno");
         System.out.println("FRONTEND_URL: " + System.getenv("FRONTEND_URL"));
+        System.out.println("MYSQL_HOST: " + System.getenv("MYSQL_HOST"));
+        System.out.println("MYSQL_PORT: " + System.getenv("MYSQL_PORT"));
+        System.out.println("MYSQL_USER: " + System.getenv("MYSQL_USER"));
+        System.out.println("MYSQL_PASSWORD: " + System.getenv("MYSQL_PASSWORD"));
     }
 }
